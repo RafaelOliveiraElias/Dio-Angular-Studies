1e31bfbb33410a3c887ba2e7ec960f8f28749450
RafaelOliveiraElias <rafaelias567@gmail.com>
2022-06-09 09:39:26 -0300

    rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,35 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { CourseListComponent } from './courses/course-list.component';
 import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './error-404/error-404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseListComponent,
-    StarComponent
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch:'full'
+      },
+      {
+        path: 'courses', component: CourseListComponent,
+      },
+      // quadro não encontra url/rota:
+      {
+        path:'**', component: Error404Component,
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,35 +1,32 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
+import { CourseService } from "./course.service";
 
 @Component({
-  selector: 'app-course-list',
   templateUrl: './course-list.component.html'
 })
 
 export class CourseListComponent implements OnInit{
-  courses: Course[] = [];
+
+  filteredCourses: Course[] = [];
+  _courses: Course[] = [];
+
+  _filteBy!: string;
+
+  constructor(private courseService: CourseService) { }
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular forms',
-        imageUrl: '/assets/images/forms.png',
-        price: 99.99,
-        code: 'XPS-8796',
-        duration: 120,
-        rating: 4.5,
-        releaseDate: 'November, 2, 2019'
-      },
-      {
-        id: 2,
-        name: 'Angular http',
-        imageUrl: '/assets/images/http.png',
-        price: 45.99,
-        code: 'LKL-1094',
-        duration: 80,
-        rating: 4,
-        releaseDate: 'November, 12, 2019'
-      }
-    ]
+    this._courses = this.courseService.retrieveAll();
+    this.filteredCourses = this._courses;
+  }
+
+  set filter(value: string) {
+    this._filteBy = value;
+
+    this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filteBy.toLocaleLowerCase()) > -1 )
+  }
+
+  get filter() {
+    return this._filteBy;
   }
 }
